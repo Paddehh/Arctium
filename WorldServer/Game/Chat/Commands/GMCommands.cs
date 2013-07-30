c4af3363c1b613ec59097d59f70a588f047c6b33
Patrick parker <paddyparker@live.com>
2013-07-31 00:02:11 +0100

    Added ban in-game command
    Please bare in mind.. im learning whilst doing this.. if i'm 'using' any
    un needed namespaces etc please inform me :)

---
+++ WorldServer/Game/Chat/Commands/GMCommands.cs
@@ -0,0 +1,48 @@
+﻿using Framework.Console.Commands;
+using Framework.Constants;
+using Framework.ObjectDefines;
+using WorldServer.Game.Packets.PacketHandler;
+using WorldServer.Game.Spawns;
+using WorldServer.Game.WorldEntities;
+using WorldServer.Network;
+using Framework.Database;
+using Framework.Constants.Authentication;
+namespace WorldServer.Game.Chat.Commands
+{
+    public class GMCommands : Globals
+    {
+        
+        [ChatCommand("ban", "")]
+        public static void Ban(string[] args, WorldClass session)
+        {
+            var pName = CommandParser.Read<string>(args, 1);
+            ChatMessageValues ChatMessage = new ChatMessageValues(0, "");
+
+            SQLResult result = DB.Characters.Select("SELECT * FROM characters WHERE name = ?", pName);
+            if (result.Count == 0)
+            {
+
+                ChatMessage.Message = "Could not find character with that name";
+                ChatHandler.SendMessage(ref session, ChatMessage);
+            }
+            else
+            {
+                var AccId = result.Read<string>(0, "accountid");
+                DB.Realms.Execute("UPDATE accounts SET banned = 1 WHERE id = ?", AccId);
+   
+                ChatMessage.Message = "Successfully banned account";
+                ChatHandler.SendMessage(ref session, ChatMessage);
+            }
+
+
+        }
+        [ChatCommand("unban", "")]
+        public static void Unban(string[] args, WorldClass session)
+        {
+            ChatMessageValues ChatMessage = new ChatMessageValues(0, "");
+            ChatMessage.Message = "Command not yet implemented";
+            ChatHandler.SendMessage(ref session, ChatMessage);
+            
+        }
+    }
+}
